590bd56e3cc2030b7dddea64d094f9963ca47b33
陆勇江 <1091064779@qq.com>
2017-04-19 10:04:21 +0800

    测试1

---
+++ app/src/main/java/com/bbtest/MainActivity.java
@@ -0,0 +1,37 @@
+package com.bbtest;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView tvAnim;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        checkBoxView = (ImageView) findViewById(R.id.checkbox);
+//        checkBoxView.setOnClickListener(this);
+    }
+
+    private boolean isChecked;
+    ImageView checkBoxView;
+
+    void onClick() {
+        isChecked = !isChecked;
+        final int[] stateSet = {android.R.attr.state_checked * (isChecked ? 1 : -1)};
+//        radioButtonView.setImageState(stateSet, true);
+        checkBoxView.setImageState(stateSet, true);
+//        expandCollapseView.setImageState(stateSet, true);
+    }
+
+    @Override
+    public void onClick(View v) {
+        onClick();
+    }
+}
